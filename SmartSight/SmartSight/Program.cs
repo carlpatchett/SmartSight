07b363a54d24e17484c0b605991af03961c7aff5
Carl Patchett <carl.patchett@hotmail.co.uk>
2020-01-20 18:14:49 +0000

    EmguCV nuget installed, working image display.

---
+++ SmartSight/SmartSight/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartSight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
